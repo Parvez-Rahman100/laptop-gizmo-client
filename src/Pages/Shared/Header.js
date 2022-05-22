d689373c968a5dde4288e97e194081ba5dee019b
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-22 19:56:43 +0600

    hero section added

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   
 </>
     return (
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-base-100 container mx-auto">
       <div className="navbar-start">
           <div className="dropdown">
               <label tabIndex="0" className="btn btn-ghost lg:hidden">
@@ -23,16 +23,12 @@ const Header = () => {
           </div>
           <Link to="/home" className="btn btn-ghost normal-case text-xl">Laptop Gizmo</Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className="navbar-end hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
               {menuItems}
           </ul>
       </div>
-      <div className="navbar-end">
-          <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-      </div>
+      
   </div>
     );
 };
