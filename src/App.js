80e2b6780882ac19c8336549aa1ffaa6dbd0f489
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-22 18:28:54 +0600

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-2xl text-green-500'>Starting project</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+    daisyui: {
+      themes: [
+        {
+          gizmo: {
+            primary: "#10ac84",
+            secondary: "#2e86de",
+            accent: "#37cdbe",
+            neutral: "#3d4451",
+            "base-100": "#ffffff",
+          },
+        },
+        "dark",
+        "cupcake",
+      ],
+    },
+  
   plugins: [require("daisyui")],
 }
