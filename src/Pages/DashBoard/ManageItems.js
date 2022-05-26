176150a0824af59c8a95017fc54cf5b6a70e453f
Parvez-Rahman100 <iamparvezrahman@gmail.com>
2022-05-26 22:06:29 +0600

    fetch link change

--- src/Pages/DashBoard/ManageItems.js
+++ src/Pages/DashBoard/ManageItems.js
@@ -8,7 +8,7 @@ const ManageItems = () => {
 
 
     const onSubmit = data => {
-        fetch('http://localhost:5000/parts', {
+        fetch('https://laptopgizmo.herokuapp.com/parts', {
             method: 'POST',
             headers: {
                 "content-type": "application/json"
