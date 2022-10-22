58cb82f77acb20f2ecac77414df3e32d2a076b88
Parvez Miah <96688877+Parvez-Rahman100@users.noreply.github.com>
2022-10-22 21:55:20 +0600

    registration page updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
-    useEffect( () =>{
+    useEffect(() => {
         if (token) {
             navigate(from, { replace: true });
         }
@@ -32,7 +32,7 @@ const Login = () => {
         return <Loading></Loading>
     }
 
-    if(error || gError){
+    if (error || gError) {
         toast.error('Something went wrong')
     }
 
@@ -41,72 +41,72 @@ const Login = () => {
     }
 
     return (
-        <div  className='flex h-screen justify-center backgroundImg items-center'>
-        <div className="card w-96  bg-transparent text-white shadow-2xl">
-            <div className="card-body">
-                <h2 className="text-center text-2xl font-bold">Login</h2>
-                <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='flex h-screen justify-center backgroundImg items-center'>
+            <div className="card w-96  bg-transparent text-white shadow-2xl">
+                <div className="card-body">
+                    <h2 className="text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                            <span className="label-text text-white">Email</span>
-                        </label>
-                        <input
-                            type="email"
-                            placeholder="Your Email"
-                            className="input text-black input-bordered w-full max-w-xs"
-                            {...register("email", {
-                                required: {
-                                    value: true,
-                                    message: 'Email is Required'
-                                },
-                                pattern: {
-                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                                    message: 'Provide a valid Email'
-                                }
-                            })}
-                        />
-                        <label className="label">
-                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                        </label>
-                    </div>
-                    <div className="form-control w-full max-w-xs">
-                        <label className="label">
-                            <span className="label-text text-white">Password</span>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="input input-bordered text-black w-full max-w-xs"
-                            {...register("password", {
-                                required: {
-                                    value: true,
-                                    message: 'Password is Required'
-                                },
-                                minLength: {
-                                    value: 6,
-                                    message: 'Must be 6 characters or longer'
-                                }
-                            })}
-                        />
-                        <label className="label">
-                            {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                            {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                        </label>
-                    </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-white">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                className="input text-black input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: 'Email is Required'
+                                    },
+                                    pattern: {
+                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                        message: 'Provide a valid Email'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            </label>
+                        </div>
+                        <div className="form-control w-full max-w-xs">
+                            <label className="label">
+                                <span className="label-text text-white">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="input input-bordered text-black w-full max-w-xs"
+                                {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: 'Password is Required'
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Must be 6 characters or longer'
+                                    }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                            </label>
+                        </div>
 
-                    
-                    <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
-                </form>
-                <p><small>New to Laptop Gizmo?  <Link className='text-black' to="/signup">Create New Account</Link></small></p>
-                <div className="divider">OR</div>
-                <button
-                    onClick={() => signInWithGoogle()}
-                    className="btn btn-outline text-white">SignIn with Google</button>
+
+                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                    </form>
+                    <p><small>New to Laptop Gizmo?  <Link className='text-black' to="/signup">Create New Account</Link></small></p>
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline text-white">SignIn with Google</button>
+                </div>
             </div>
-        </div>
-    </div >
+        </div >
     );
 };
 
